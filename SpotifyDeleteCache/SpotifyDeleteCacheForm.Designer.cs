f0d6af9ea0be6e55b3ce68eac4123714148dcb80
David <3614449+GenesisFR@users.noreply.github.com>
2018-04-15 21:55:47 -0400

    + added Report bug and Refresh buttons * fixed several crashes if the cache folder was missing - moderate code refactoring

--- SpotifyDeleteCache/SpotifyDeleteCacheForm.Designer.cs
+++ SpotifyDeleteCache/SpotifyDeleteCacheForm.Designer.cs
@@ -39,6 +39,8 @@
             this.buttonOpenCacheFolder = new System.Windows.Forms.Button();
             this.linkLabelAuthor = new System.Windows.Forms.LinkLabel();
             this.pictureBoxKappa = new System.Windows.Forms.PictureBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonReportBug = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKappa)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,18 +55,19 @@
             // 
             // textBoxCacheLocation
             // 
-            this.textBoxCacheLocation.Location = new System.Drawing.Point(175, 7);
+            this.textBoxCacheLocation.Location = new System.Drawing.Point(98, 7);
             this.textBoxCacheLocation.Name = "textBoxCacheLocation";
             this.textBoxCacheLocation.ReadOnly = true;
-            this.textBoxCacheLocation.Size = new System.Drawing.Size(400, 20);
+            this.textBoxCacheLocation.Size = new System.Drawing.Size(350, 20);
             this.textBoxCacheLocation.TabIndex = 1;
             // 
             // buttonDeleteCache
             // 
-            this.buttonDeleteCache.Location = new System.Drawing.Point(298, 145);
+            this.buttonDeleteCache.AutoSize = true;
+            this.buttonDeleteCache.Location = new System.Drawing.Point(296, 143);
             this.buttonDeleteCache.Name = "buttonDeleteCache";
-            this.buttonDeleteCache.Size = new System.Drawing.Size(79, 23);
-            this.buttonDeleteCache.TabIndex = 8;
+            this.buttonDeleteCache.Size = new System.Drawing.Size(81, 23);
+            this.buttonDeleteCache.TabIndex = 10;
             this.buttonDeleteCache.Text = "Delete cache";
             this.buttonDeleteCache.UseVisualStyleBackColor = true;
             this.buttonDeleteCache.Click += new System.EventHandler(this.ButtonDeleteCache_Click);
@@ -75,17 +78,17 @@
             this.labelDirectoryAge.Location = new System.Drawing.Point(10, 70);
             this.labelDirectoryAge.Name = "labelDirectoryAge";
             this.labelDirectoryAge.Size = new System.Drawing.Size(122, 13);
-            this.labelDirectoryAge.TabIndex = 3;
+            this.labelDirectoryAge.TabIndex = 4;
             this.labelDirectoryAge.Text = "Delete folders older than";
             // 
             // dateTimePickerDirectoryAge
             // 
             this.dateTimePickerDirectoryAge.CustomFormat = "MMMM dd, yyyy - HH:mm";
             this.dateTimePickerDirectoryAge.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePickerDirectoryAge.Location = new System.Drawing.Point(175, 66);
+            this.dateTimePickerDirectoryAge.Location = new System.Drawing.Point(142, 66);
             this.dateTimePickerDirectoryAge.Name = "dateTimePickerDirectoryAge";
             this.dateTimePickerDirectoryAge.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePickerDirectoryAge.TabIndex = 4;
+            this.dateTimePickerDirectoryAge.TabIndex = 5;
             this.dateTimePickerDirectoryAge.ValueChanged += new System.EventHandler(this.DateTimePickerDirectoryAge_ValueChanged);
             // 
             // labelTotalCacheSize
@@ -93,25 +96,26 @@
             this.labelTotalCacheSize.AutoSize = true;
             this.labelTotalCacheSize.Location = new System.Drawing.Point(10, 40);
             this.labelTotalCacheSize.Name = "labelTotalCacheSize";
-            this.labelTotalCacheSize.Size = new System.Drawing.Size(121, 13);
-            this.labelTotalCacheSize.TabIndex = 2;
-            this.labelTotalCacheSize.Text = "Total cache size: 0.0GB";
+            this.labelTotalCacheSize.Size = new System.Drawing.Size(116, 13);
+            this.labelTotalCacheSize.TabIndex = 3;
+            this.labelTotalCacheSize.Text = "Total cache size: 0.0 B";
             // 
             // labelDeletedCacheSize
             // 
             this.labelDeletedCacheSize.AutoSize = true;
             this.labelDeletedCacheSize.Location = new System.Drawing.Point(10, 100);
             this.labelDeletedCacheSize.Name = "labelDeletedCacheSize";
-            this.labelDeletedCacheSize.Size = new System.Drawing.Size(134, 13);
-            this.labelDeletedCacheSize.TabIndex = 5;
-            this.labelDeletedCacheSize.Text = "Deleted cache size: 0.0GB";
+            this.labelDeletedCacheSize.Size = new System.Drawing.Size(129, 13);
+            this.labelDeletedCacheSize.TabIndex = 6;
+            this.labelDeletedCacheSize.Text = "Deleted cache size: 0.0 B";
             // 
             // buttonOpenCacheFolder
             // 
-            this.buttonOpenCacheFolder.Location = new System.Drawing.Point(175, 145);
+            this.buttonOpenCacheFolder.AutoSize = true;
+            this.buttonOpenCacheFolder.Location = new System.Drawing.Point(468, 5);
             this.buttonOpenCacheFolder.Name = "buttonOpenCacheFolder";
-            this.buttonOpenCacheFolder.Size = new System.Drawing.Size(103, 23);
-            this.buttonOpenCacheFolder.TabIndex = 7;
+            this.buttonOpenCacheFolder.Size = new System.Drawing.Size(105, 24);
+            this.buttonOpenCacheFolder.TabIndex = 2;
             this.buttonOpenCacheFolder.Text = "Open cache folder";
             this.buttonOpenCacheFolder.UseVisualStyleBackColor = true;
             this.buttonOpenCacheFolder.Click += new System.EventHandler(this.ButtonOpenCacheFolder_Click);
@@ -120,13 +124,13 @@
             // 
             this.linkLabelAuthor.AllowDrop = true;
             this.linkLabelAuthor.AutoSize = true;
-            this.linkLabelAuthor.LinkArea = new System.Windows.Forms.LinkArea(8, 7);
-            this.linkLabelAuthor.Location = new System.Drawing.Point(10, 150);
+            this.linkLabelAuthor.LinkArea = new System.Windows.Forms.LinkArea(16, 7);
+            this.linkLabelAuthor.Location = new System.Drawing.Point(98, 149);
             this.linkLabelAuthor.Name = "linkLabelAuthor";
-            this.linkLabelAuthor.Size = new System.Drawing.Size(92, 17);
-            this.linkLabelAuthor.TabIndex = 44;
+            this.linkLabelAuthor.Size = new System.Drawing.Size(130, 17);
+            this.linkLabelAuthor.TabIndex = 8;
             this.linkLabelAuthor.TabStop = true;
-            this.linkLabelAuthor.Text = "Made by Genesis";
+            this.linkLabelAuthor.Text = "v1.02 - Made by Genesis";
             this.linkLabelAuthor.UseCompatibleTextRendering = true;
             this.linkLabelAuthor.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabelAuthor_LinkClicked);
             // 
@@ -144,11 +148,36 @@
             this.pictureBoxKappa.WaitOnLoad = true;
             this.pictureBoxKappa.Click += new System.EventHandler(this.PictureBoxKappa_Click);
             // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.AutoSize = true;
+            this.buttonRefresh.Location = new System.Drawing.Point(234, 144);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(54, 23);
+            this.buttonRefresh.TabIndex = 9;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.ButtonRefresh_Click);
+            // 
+            // buttonReportBug
+            // 
+            this.buttonReportBug.AutoSize = true;
+            this.buttonReportBug.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonReportBug.Location = new System.Drawing.Point(13, 143);
+            this.buttonReportBug.Name = "buttonReportBug";
+            this.buttonReportBug.Size = new System.Drawing.Size(79, 23);
+            this.buttonReportBug.TabIndex = 7;
+            this.buttonReportBug.Text = "Report a bug";
+            this.buttonReportBug.UseVisualStyleBackColor = true;
+            this.buttonReportBug.Click += new System.EventHandler(this.ButtonReportBug_Click);
+            // 
             // SpotifyDeleteCacheForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 176);
+            this.Controls.Add(this.buttonReportBug);
+            this.Controls.Add(this.buttonRefresh);
             this.Controls.Add(this.pictureBoxKappa);
             this.Controls.Add(this.linkLabelAuthor);
             this.Controls.Add(this.buttonOpenCacheFolder);
@@ -163,6 +192,7 @@
             this.Name = "SpotifyDeleteCacheForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SpotifyDeleteCache";
+            this.Load += new System.EventHandler(this.SpotifyDeleteCacheForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKappa)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -181,6 +211,8 @@
         private System.Windows.Forms.Button buttonOpenCacheFolder;
         private System.Windows.Forms.LinkLabel linkLabelAuthor;
         private System.Windows.Forms.PictureBox pictureBoxKappa;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonReportBug;
     }
 }
 
--- SpotifyDeleteCache/SpotifyDeleteCacheForm.cs
+++ SpotifyDeleteCache/SpotifyDeleteCacheForm.cs
@@ -8,15 +8,24 @@ namespace SpotifyDeleteCache
 {
     public partial class SpotifyDeleteCacheForm : Form
     {
+        private bool _isRefresh; // used to avoid showing more than one error message when clicking the Refresh button
         private string _spotifyCacheLocation;
-        private string[] _cacheDirectoriesToDelete = new string[] { };
+        private string[] _cacheDirectoriesToDelete;
 
         public SpotifyDeleteCacheForm()
         {
             InitializeComponent();
 
-            FindSpotifyCacheLocation();
-            UpdateCacheSize(true);
+            // Get cache folder location
+            string localAppData   = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            _spotifyCacheLocation = Path.Combine(localAppData, @"Spotify\Data");
+
+            _cacheDirectoriesToDelete = new string[] { };
+    }
+
+    private void SpotifyDeleteCacheForm_Load(object sender, EventArgs e)
+        {
+            buttonRefresh.PerformClick();
         }
 
         private void ComputeTotalCacheSize()
@@ -29,44 +38,87 @@ namespace SpotifyDeleteCache
             labelDeletedCacheSize.Text = $"Deleted cache size: { ReadableBytes(GetDirectorySize(_spotifyCacheLocation, false)) }";
         }
 
-        private void DeleteCacheDirectories()
+        private bool DeleteCacheDirectories()
         {
+            bool allFoldersDeleted = true;
+
             foreach (string directory in _cacheDirectoriesToDelete)
             {
                 try
                 {
                     Directory.Delete(directory, true);
                 }
-                catch (Exception)
+                catch
                 {
-                    // Nothing to do here
+                    allFoldersDeleted = false;
                 }
             }
+
+            return allFoldersDeleted;
         }
 
-        private void FindSpotifyCacheLocation()
+        private bool FindSpotifyCacheLocation()
         {
-            string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            _spotifyCacheLocation = Path.Combine(localAppData, @"Spotify\Data");
-            textBoxCacheLocation.Text = _spotifyCacheLocation;
+            // Check if cache folder exists
+            bool spotifyCacheFolderExists = Directory.Exists(_spotifyCacheLocation);
+
+            // Update textboxes
+            textBoxCacheLocation.Text  = _spotifyCacheLocation;
+            labelTotalCacheSize.Text   = "Total cache size: 0.0 B";
+            labelDeletedCacheSize.Text = "Deleted cache size: 0.0 B";
+
+            // Toggle elements
+            buttonOpenCacheFolder.Enabled      = spotifyCacheFolderExists;
+            dateTimePickerDirectoryAge.Enabled = spotifyCacheFolderExists;
+            buttonDeleteCache.Enabled          = spotifyCacheFolderExists;
+
+            return spotifyCacheFolderExists;
         }
 
         private long GetDirectorySize(string path, bool computeTotalSize = true)
         {
-            string[] a = computeTotalSize ? Directory.GetFiles(path, "*.*", SearchOption.AllDirectories) : _cacheDirectoriesToDelete;
             long size = 0;
 
-            foreach (string name in a)
-                size += computeTotalSize ? new FileInfo(name).Length : GetDirectorySize(name);
+            if (computeTotalSize)
+            {
+                string[] filenames;
+
+                try
+                {
+                    filenames = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+                    foreach (string filename in filenames)
+                        size += new FileInfo(filename).Length;
+                }
+                catch
+                {
+                    // Nothing to do here
+                }
+            }
+            else
+            {
+                foreach (string directory in _cacheDirectoriesToDelete)
+                    size += GetDirectorySize(directory);
+            }
 
             return size;
         }
 
         private string[] GetOldCacheDirectories()
         {
-            string[] oldCacheDirectories = Directory.GetDirectories(_spotifyCacheLocation)
-                                           .Where(directory => new DirectoryInfo(directory).LastWriteTime < dateTimePickerDirectoryAge.Value)
-                                           .Select(directory => directory).ToArray();
+            string[] oldCacheDirectories = new string[] { };
+
+            try
+            {
+                oldCacheDirectories = Directory.GetDirectories(_spotifyCacheLocation)
+                                      .Where(directory => new DirectoryInfo(directory).LastWriteTime < dateTimePickerDirectoryAge.Value)
+                                      .Select(directory => directory).ToArray();
+            }
+            catch
+            {
+                // Nothing to do here
+            }
+
             return oldCacheDirectories;
         }
 
@@ -81,11 +133,72 @@ namespace SpotifyDeleteCache
 
         private void UpdateCacheSize(bool computeTotalSize = false)
         {
-            if (computeTotalSize)
-                ComputeTotalCacheSize();
+            if (Directory.Exists(_spotifyCacheLocation))
+            {
+                _cacheDirectoriesToDelete = GetOldCacheDirectories();
 
-            _cacheDirectoriesToDelete = GetOldCacheDirectories();
-            ComputeDeletedCacheSize();
+                if (computeTotalSize)
+                    ComputeTotalCacheSize();
+
+                ComputeDeletedCacheSize();
+            }
+            else if (!_isRefresh)
+                MessageBox.Show("The Spofify cache folder no longer exists!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        // Event handlers
+        private void ButtonDeleteCache_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(_spotifyCacheLocation))
+            {
+                string text;
+                MessageBoxIcon icon;
+
+                if (DeleteCacheDirectories())
+                {
+                    text = "The Spofify cache has been deleted!";
+                    icon = MessageBoxIcon.Information;
+                }
+                else
+                {
+                    text = "The Spofify cache has been deleted, but some folder couldn't be deleted.";
+                    icon = MessageBoxIcon.Warning;
+                }
+
+                MessageBox.Show(text, "Success", MessageBoxButtons.OK, icon);
+                UpdateCacheSize(true);
+            }
+            else if (!_isRefresh)
+                MessageBox.Show("The Spofify cache folder no longer exists!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void ButtonOpenCacheFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(_spotifyCacheLocation);
+            }
+            catch
+            {
+                MessageBox.Show("The Spofify cache folder no longer exists!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void ButtonRefresh_Click(object sender, EventArgs e)
+        {
+            _isRefresh = true;
+
+            if (FindSpotifyCacheLocation())
+                UpdateCacheSize(true);
+            else
+                MessageBox.Show("The Spofify cache folder wasn't found!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            _isRefresh = false;
+        }
+
+        private void ButtonReportBug_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/GenesisFR/SpotifyDeleteCache/issues");
         }
 
         private void DateTimePickerDirectoryAge_ValueChanged(object sender, EventArgs e)
@@ -96,18 +209,6 @@ namespace SpotifyDeleteCache
         private void LinkLabelAuthor_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             Process.Start("https://github.com/GenesisFR");
-        }
-
-        private void ButtonOpenCacheFolder_Click(object sender, EventArgs e)
-        {
-            Process.Start(_spotifyCacheLocation);
-        }
-
-        private void ButtonDeleteCache_Click(object sender, EventArgs e)
-        {
-            DeleteCacheDirectories();
-            UpdateCacheSize(true);
-            MessageBox.Show("Spofify cache deleted!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void PictureBoxKappa_Click(object sender, EventArgs e)
